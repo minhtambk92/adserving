70c85ebaa592519b444f81e61d50ed626cdde28e
Manh Pham <manhhailua@gmail.com>
2016-10-13 17:21:39 +0700

    change 404 page layout

--- src/routes/notFound/NotFound.js
+++ src/routes/notFound/NotFound.js
@@ -10,16 +10,36 @@
 import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import Layout from '../../components/Layout';
+import Link from '../../components/Link';
 import s from './NotFound.css';
+
+const pageTitle = '404';
 
 function NotFound({ title }) {
   return (
-    <Layout full={false}>
-      <div className={s.root}>
-        <div className={s.container}>
-          <h1>{title}</h1>
-          <p>Sorry, the page you were trying to view does not exist.</p>
+    <Layout pageTitle={pageTitle} pageSubTitle={title}>
+      <div>
+        <div className="error-page">
+          <h2 className="headline text-yellow"> 404</h2>
+          <div className="error-content">
+            <h3><i className="fa fa-warning text-yellow" /> Oops! Page not found.</h3>
+            <p>We could not find the page you were looking for. Meanwhile,
+              you may <Link to="/">return to dashboard</Link> or try using the search form.</p>
+            <form className="search-form">
+              <div className="input-group">
+                <input type="text" name="search" className="form-control" placeholder="Search" />
+                <div className="input-group-btn">
+                  <button
+                    type="submit" name="submit" className="btn btn-warning btn-flat"
+                  ><i className="fa fa-search" /></button>
+                </div>
+              </div>
+              {/* /.input-group */}
+            </form>
+          </div>
+          {/* /.error-content */}
         </div>
+        {/* /.error-page */}
       </div>
     </Layout>
   );
