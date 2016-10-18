10c582958b98e18bef499f6d51e33afba5e16cbf
Manh Pham <manhhailua@gmail.com>
2016-10-18 16:42:08 +0700

    separate by comments

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 
+// Default constants
 export const SET_RUNTIME_VARIABLE = 'SET_RUNTIME_VARIABLE';
 export const SET_LOCALE_START = 'SET_LOCALE_START';
 export const SET_LOCALE_SUCCESS = 'SET_LOCALE_SUCCESS';
@@ -8,9 +9,11 @@ export const FETCH_CONTENT_START = 'FETCH_CONTENT_START';
 export const FETCH_CONTENT_SUCCESS = 'FETCH_CONTENT_SUCCESS';
 export const FETCH_CONTENT_ERROR = 'FETCH_CONTENT_ERROR';
 
+// Model states
 export const STATUS_ACTIVE = 'active';
 export const STATUS_INACTIVE = 'inactive';
 
+// Redux actions
 export const GET_SITE = 'GET_SITE';
 export const GET_SITES = 'GET_SITES';
 export const CREATE_SITE = 'CREATE_SITE';
