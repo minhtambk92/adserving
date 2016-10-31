26434ad99378c9c8f6c1e5bf17e5e5f40a87c779
Manh Pham <manhhailua@gmail.com>
2016-10-31 13:11:32 +0700

    redirect if logged in

--- src/server/middlewares/index.js
+++ src/server/middlewares/index.js
@@ -9,6 +9,14 @@ const middleware = express();
 
 middleware.use(unauthorizedRoutes);
 
+middleware.use(['/login', '/register', '/logout'], (req, res, next) => {
+  if (req.cookies.id_token) {
+    return res.redirect('/');
+  }
+
+  return next();
+});
+
 middleware.use(/^\/((?!(login|register|logout)$).)*$/, (req, res, next) => {
   // Check user token for user login state
   if (!req.cookies.id_token) {
