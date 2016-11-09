48530922c8d6dc6a2dca995730644bb6e838b92e
Manh Pham <manhhailua@gmail.com>
2016-11-09 11:40:02 +0700

    separate create form

---
+++ src/routes/resources/types/CreateResourceForm.js
@@ -0,0 +1,80 @@
+/**
+ * Created by Manhhailua on 11/9/16.
+ */
+
+import React, { Component, Proptypes } from 'react';
+
+class CreateResourceForm extends Component {
+
+  static propTypes = {
+    createResource: Proptypes.func.isRequired,
+  };
+
+  clearInput() {
+    this.inputResourceUniqueName.value = null;
+    this.inputResourceName.value = null;
+  }
+
+  createResource() {
+    const uniqueName = this.inputResourceUniqueName.value;
+    const name = this.inputResourceName.value;
+
+    if (uniqueName && name) {
+      this.props.createResource({ uniqueName, name });
+      this.clearInput();
+    }
+  }
+
+  render() {
+    return (
+      <form className="form-horizontal">
+        <div className="box-body">
+          <div className="form-group">
+            <label
+              htmlFor="inputResourceUniqueName" className="col-sm-2 control-label"
+            >Unique name</label>
+            <div className="col-sm-10">
+              <input
+                type="text" className="form-control" id="inputResourceUniqueName"
+                placeholder="user"
+                ref={c => {
+                  this.inputResourceUniqueName = c;
+                }}
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <label
+              htmlFor="inputResourceName" className="col-sm-2 control-label"
+            >Display name</label>
+            <div className="col-sm-10">
+              <input
+                type="text" className="form-control" id="inputResourceName"
+                placeholder="User"
+                ref={c => {
+                  this.inputResourceName = c;
+                }}
+              />
+            </div>
+          </div>
+        </div>
+        {/* /.box-body */}
+        <div className="box-footer">
+          {/* eslint-disable jsx-a11y/no-static-element-interactions */}
+          <a
+            className="btn btn-app pull-right"
+            onClick={event => this.clearInput(event)}
+          ><i className="fa fa-eraser" /> Clear</a>
+          <a
+            className="btn btn-app pull-right"
+            onClick={event => this.createResource(event)}
+          ><i className="fa fa-check" /> Confirm</a>
+          {/* eslint-enable jsx-a11y/no-static-element-interactions */}
+        </div>
+        {/* /.box-footer */}
+      </form>
+    );
+  }
+}
+
+export default CreateResourceForm;
--- src/routes/resources/types/Resources.js
+++ src/routes/resources/types/Resources.js
@@ -14,6 +14,7 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import { getResources, createResource } from '../../../actions/resources';
 import { setUsersFilters } from '../../../actions/users';
 import Layout from '../../../components/Layout';
+import CreateResourceForm from './CreateResourceForm';
 import ResourceList from './ResourceList';
 import s from './Resources.css';
 
@@ -41,21 +42,6 @@ class Resources extends Component {
     this.props.getResources();
   }
 
-  clearInput() {
-    this.inputResourceUniqueName.value = null;
-    this.inputResourceName.value = null;
-  }
-
-  createResource() {
-    const uniqueName = this.inputResourceUniqueName.value;
-    const name = this.inputResourceName.value;
-
-    if (uniqueName && name) {
-      this.props.createResource({ uniqueName, name });
-      this.clearInput();
-    }
-  }
-
   render() {
     return (
       <Layout pageTitle={pageTitle} pageSubTitle={pageSubTitle}>
@@ -75,52 +61,7 @@ class Resources extends Component {
                 </div>
                 {/* /.box-header */}
                 {/* form start */}
-                <form className="form-horizontal">
-                  <div className="box-body">
-                    <div className="form-group">
-                      <label
-                        htmlFor="inputResourceUniqueName" className="col-sm-2 control-label"
-                      >Unique name</label>
-                      <div className="col-sm-10">
-                        <input
-                          type="text" className="form-control" id="inputResourceUniqueName"
-                          placeholder="user"
-                          ref={c => {
-                            this.inputResourceUniqueName = c;
-                          }}
-                        />
-                      </div>
-                    </div>
-                    <div className="form-group">
-                      <label
-                        htmlFor="inputResourceName" className="col-sm-2 control-label"
-                      >Display name</label>
-                      <div className="col-sm-10">
-                        <input
-                          type="text" className="form-control" id="inputResourceName"
-                          placeholder="User"
-                          ref={c => {
-                            this.inputResourceName = c;
-                          }}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  {/* /.box-body */}
-                  <div className="box-footer">
-                    {/* eslint-disable jsx-a11y/no-static-element-interactions */}
-                    <a
-                      className="btn btn-app pull-right"
-                      onClick={event => this.clearInput(event)}
-                    ><i className="fa fa-eraser" /> Clear</a>
-                    <a
-                      className="btn btn-app pull-right"
-                      onClick={event => this.createResource(event)}
-                    ><i className="fa fa-check" /> Confirm</a>
-                    {/* eslint-enable jsx-a11y/no-static-element-interactions */}
-                  </div>
-                  {/* /.box-footer */}
-                </form>
+                <CreateResourceForm createResource={this.createResource} />
               </div>
               {/* /.col */}
             </section>
