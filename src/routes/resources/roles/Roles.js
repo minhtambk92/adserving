05786dea3b340f789289680160dae24958d189d6
Manh Pham <manhhailua@gmail.com>
2016-11-09 17:23:23 +0700

    remove filter component

--- src/routes/resources/roles/Roles.js
+++ src/routes/resources/roles/Roles.js
@@ -15,7 +15,6 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import { getRoles, createRole, getRolesFilters, setRolesFilters } from '../../../actions/roles';
 import { setUsersFilters } from '../../../actions/users';
 import Layout from '../../../components/Layout';
-import FilterRoleForm from './FilterRoleForm';
 import CreateRoleForm from './CreateRoleForm';
 import RoleList from './RoleList';
 import s from './Roles.css';
@@ -57,28 +56,6 @@ class Roles extends Component {
     return (
       <Layout pageTitle={pageTitle} pageSubTitle={pageSubTitle}>
         <div>
-
-          <div className="row">
-            <section className="col-lg-12">
-              {/* BOX: FILTER */}
-              <div className="box box-default">
-                <div className="box-header with-border">
-                  <h3 className="box-title">Filter by:</h3>
-                  <div className="box-tools pull-right">
-                    <button type="button" className="btn btn-box-tool" data-widget="collapse">
-                      <i className="fa fa-minus" />
-                    </button>
-                  </div>
-                </div>
-                {/* /.box-header */}
-                <FilterRoleForm
-                  filters={this.props.roles.filters}
-                  setRolesFilters={this.props.setRolesFilters}
-                />
-              </div>
-              {/* /.col */}
-            </section>
-          </div>
 
           <div className="row">
             <section className="col-lg-12">
