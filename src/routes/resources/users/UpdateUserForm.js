67bab4ebb6733975d105e81ef8f46c1d26ba844b
Manh Pham <manhhailua@gmail.com>
2016-11-11 09:42:22 +0700

    separate update form

---
+++ src/routes/resources/users/UpdateUserForm.js
@@ -0,0 +1,228 @@
+/**
+ * Created by Manhhailua on 11/11/16.
+ */
+
+import React, { Component, PropTypes } from 'react';
+import _ from 'lodash';
+import Link from '../../../components/Link';
+import { Select2 } from '../../../components/UI';
+
+class UpdateUserForm extends Component {
+
+  static propTypes = {
+    userId: PropTypes.string.isRequired,
+    userEditing: PropTypes.object.isRequired,
+    roleList: PropTypes.array.isRequired,
+    updateUser: PropTypes.func.isRequired,
+    deleteUser: PropTypes.func.isRequired,
+  };
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      currentRoles: [],
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const {
+      email,
+      roles,
+      emailConfirmed,
+      status,
+    } = nextProps.users.editing;
+
+    this.inputUserEmail.value = email;
+    this.setState({ currentRoles: roles.map(role => role.uniqueName).sort() });
+    this.inputUserEmailConfirmed.value = emailConfirmed;
+    this.inputUserStatus.value = status;
+  }
+
+  updateUser() {
+    const { userEditing, roleList } = this.props;
+
+    const email = this.inputUserEmail.value;
+    const roles = $('#inputUserRoles').val(); // eslint-disable-line no-undef
+    const password = this.inputUserPassword.value;
+    const passwordConfirmation = this.inputUserPasswordConfirmation.value;
+    const emailConfirmed = this.inputUserEmailConfirmed.value;
+    const status = this.inputUserStatus.value;
+
+    const user = { id: this.props.userId };
+
+    if (email && email !== userEditing.email) {
+      user.email = email;
+    }
+
+    if (roles && !_.isEqual(roles.sort(), this.state.currentRoles)) {
+      user.roles = JSON.stringify(roleList.map(role => ({
+        id: role.id,
+        isGranted: roles.indexOf(role.uniqueName) !== -1,
+      })));
+    }
+
+    if (password && passwordConfirmation && password === passwordConfirmation) {
+      user.password = password;
+    }
+
+    if (emailConfirmed && emailConfirmed !== userEditing.emailConfirmed.toString()) {
+      user.emailConfirmed = emailConfirmed === 'true'; // Convert to boolean
+    }
+
+    if (status && status !== userEditing.status) {
+      user.status = status;
+    }
+
+    this.props.updateUser(user);
+  }
+
+  deleteUser() {
+    this.props.deleteUser(this.props.userId);
+  }
+
+  render() {
+    const { roleList } = this.props;
+
+    return (
+      <form className="form-horizontal">
+        <div className="box-body">
+          {/* email */}
+          <div className="form-group">
+            <label
+              htmlFor="inputUserEmail"
+              className="col-sm-2 control-label"
+            >Email</label>
+            <div className="col-sm-10">
+              <input
+                type="text" className="form-control" id="inputUserEmail"
+                placeholder="contact@dantri.com.vn"
+                ref={c => {
+                  this.inputUserEmail = c;
+                }}
+              />
+            </div>
+          </div>
+          {/* role */}
+          <div className="form-group">
+            <label
+              htmlFor="inputUserRoles"
+              className="col-sm-2 control-label"
+            >Role</label>
+            <div className="col-sm-10">
+              <Select2
+                id="inputUserRoles"
+                className="form-control"
+                style={{ width: '100%' }}
+                data-placeholder="Select roles"
+                defaultValue={this.state.currentRoles}
+                multiple
+                ref={c => {
+                  this.inputUserRoles = c;
+                }}
+              >
+                <optgroup>
+                  {roleList.map(role => (
+                    <option
+                      key={role.id} value={role.uniqueName}
+                    >{role.name}</option>
+                  ))}
+                </optgroup>
+              </Select2>
+            </div>
+          </div>
+          {/* password */}
+          <div className="form-group">
+            <label
+              htmlFor="inputUserPassword"
+              className="col-sm-2 control-label"
+            >Password</label>
+            <div className="col-sm-10">
+              <input
+                type="password" className="form-control" id="inputUserPassword"
+                placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+                ref={c => {
+                  this.inputUserPassword = c;
+                }}
+              />
+            </div>
+          </div>
+          {/* passwordConfirmation */}
+          <div className="form-group">
+            <label
+              htmlFor="inputUserPasswordConfirmation"
+              className="col-sm-2 control-label"
+            >Password again</label>
+            <div className="col-sm-10">
+              <input
+                type="password" className="form-control"
+                id="inputUserPasswordConfirmation"
+                placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+                ref={c => {
+                  this.inputUserPasswordConfirmation = c;
+                }}
+              />
+            </div>
+          </div>
+          {/* emailConfirmed */}
+          <div className="form-group">
+            <label
+              htmlFor="inputUserEmailConfirmed"
+              className="col-sm-2 control-label"
+            >Email confirmed</label>
+            <div className="col-sm-10">
+              <select
+                id="inputUserEmailConfirmed"
+                className="form-control"
+                ref={c => {
+                  this.inputUserEmailConfirmed = c;
+                }}
+              >
+                <option value="true">Yes</option>
+                <option value="false">No</option>
+              </select>
+            </div>
+          </div>
+          {/* status */}
+          <div className="form-group">
+            <label
+              htmlFor="inputUserStatus"
+              className="col-sm-2 control-label"
+            >Status</label>
+            <div className="col-sm-10">
+              <select
+                id="inputUserStatus"
+                className="form-control"
+                ref={c => {
+                  this.inputUserStatus = c;
+                }}
+              >
+                <option value="active">Active</option>
+                <option value="inactive">Inactive</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        {/* /.box-body */}
+        <div className="box-footer">
+          <Link
+            to="/resource/user" className="btn btn-app pull-right"
+          ><i className="fa fa-undo" /> Cancel</Link>
+          <Link
+            to="/resource/user" className="btn btn-app pull-right"
+            onClick={event => this.deleteUser(event)}
+          ><i className="fa fa-trash-o" /> Delete</Link>
+          {/* eslint-disable jsx-a11y/no-static-element-interactions */}
+          <a
+            className="btn btn-app pull-right"
+            onClick={event => this.updateUser(event)}
+          ><i className="fa fa-floppy-o" /> Save</a>
+          {/* eslint-enable jsx-a11y/no-static-element-interactions */}
+        </div>
+        {/* /.box-footer */}
+      </form>
+    );
+  }
+}
+
+export default UpdateUserForm;
--- src/routes/resources/users/user/User.js
+++ src/routes/resources/users/user/User.js
@@ -9,14 +9,12 @@
 
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import _ from 'lodash';
 // import { defineMessages, FormattedRelative } from 'react-intl';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import { getUser, updateUser, deleteUser } from '../../../../actions/users';
 import { getRoles } from '../../../../actions/roles';
 import Layout from '../../../../components/Layout';
-import Link from '../../../../components/Link';
-import { Select2 } from '../../../../components/UI';
+import UpdateUserForm from '../UpdateUserForm';
 import s from './User.css';
 
 const pageTitle = 'User';
@@ -33,75 +31,19 @@ class User extends Component {
     deleteUser: PropTypes.func,
   };
 
-  constructor(props, context) {
-    super(props, context);
-
-    this.state = {
-      currentRoles: [],
-    };
-  }
-
   componentWillMount() {
     this.props.getRoles();
     this.props.getUser(this.props.userId);
   }
 
-  componentWillReceiveProps(nextProps) {
-    const {
-      email,
-      roles,
-      emailConfirmed,
-      status,
-    } = nextProps.users.editing;
-
-    this.inputUserEmail.value = email;
-    this.setState({ currentRoles: roles.map(role => role.uniqueName).sort() });
-    this.inputUserEmailConfirmed.value = emailConfirmed;
-    this.inputUserStatus.value = status;
-  }
-
-  updateUser() {
-    const email = this.inputUserEmail.value;
-    const roles = $('#inputUserRoles').val(); // eslint-disable-line no-undef
-    const password = this.inputUserPassword.value;
-    const passwordConfirmation = this.inputUserPasswordConfirmation.value;
-    const emailConfirmed = this.inputUserEmailConfirmed.value;
-    const status = this.inputUserStatus.value;
-
-    const user = { id: this.props.userId };
-
-    if (email && email !== this.props.users.editing.email) {
-      user.email = email;
-    }
-
-    if (roles && !_.isEqual(roles.sort(), this.state.currentRoles)) {
-      user.roles = JSON.stringify(this.props.roles.list.map(role => ({
-        id: role.id,
-        isGranted: roles.indexOf(role.uniqueName) !== -1,
-      })));
-    }
-
-    if (password && passwordConfirmation && password === passwordConfirmation) {
-      user.password = password;
-    }
-
-    if (emailConfirmed && emailConfirmed !== this.props.users.editing.emailConfirmed.toString()) {
-      user.emailConfirmed = emailConfirmed === 'true'; // Convert to boolean
-    }
-
-    if (status && status !== this.props.users.editing.status) {
-      user.status = status;
-    }
-
-    this.props.updateUser(user);
-  }
-
-  deleteUser() {
-    this.props.deleteUser(this.props.userId);
-  }
-
   render() {
-    const { roles, users } = this.props;
+    const {
+      userId,
+      roles,
+      users,
+      updateUser, // eslint-disable-line no-shadow
+      deleteUser, // eslint-disable-line no-shadow
+    } = this.props;
 
     return (
       <Layout
@@ -125,142 +67,13 @@ class User extends Component {
                   </div>
                 </div>
                 {/* /.box-header */}
-                <form className="form-horizontal">
-                  <div className="box-body">
-                    {/* email */}
-                    <div className="form-group">
-                      <label
-                        htmlFor="inputUserEmail"
-                        className="col-sm-2 control-label"
-                      >Email</label>
-                      <div className="col-sm-10">
-                        <input
-                          type="text" className="form-control" id="inputUserEmail"
-                          placeholder="contact@dantri.com.vn"
-                          ref={c => {
-                            this.inputUserEmail = c;
-                          }}
-                        />
-                      </div>
-                    </div>
-                    {/* role */}
-                    <div className="form-group">
-                      <label
-                        htmlFor="inputUserRoles"
-                        className="col-sm-2 control-label"
-                      >Role</label>
-                      <div className="col-sm-10">
-                        <Select2
-                          id="inputUserRoles"
-                          className="form-control"
-                          style={{ width: '100%' }}
-                          data-placeholder="Select roles"
-                          defaultValue={this.state.currentRoles}
-                          multiple
-                          ref={c => {
-                            this.inputUserRoles = c;
-                          }}
-                        >
-                          <optgroup>
-                            {roles.list.map(role => (
-                              <option
-                                key={role.id} value={role.uniqueName}
-                              >{role.name}</option>
-                            ))}
-                          </optgroup>
-                        </Select2>
-                      </div>
-                    </div>
-                    {/* password */}
-                    <div className="form-group">
-                      <label
-                        htmlFor="inputUserPassword"
-                        className="col-sm-2 control-label"
-                      >Password</label>
-                      <div className="col-sm-10">
-                        <input
-                          type="password" className="form-control" id="inputUserPassword"
-                          placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
-                          ref={c => {
-                            this.inputUserPassword = c;
-                          }}
-                        />
-                      </div>
-                    </div>
-                    {/* passwordConfirmation */}
-                    <div className="form-group">
-                      <label
-                        htmlFor="inputUserPasswordConfirmation"
-                        className="col-sm-2 control-label"
-                      >Password again</label>
-                      <div className="col-sm-10">
-                        <input
-                          type="password" className="form-control"
-                          id="inputUserPasswordConfirmation"
-                          placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
-                          ref={c => {
-                            this.inputUserPasswordConfirmation = c;
-                          }}
-                        />
-                      </div>
-                    </div>
-                    {/* emailConfirmed */}
-                    <div className="form-group">
-                      <label
-                        htmlFor="inputUserEmailConfirmed"
-                        className="col-sm-2 control-label"
-                      >Email confirmed</label>
-                      <div className="col-sm-10">
-                        <select
-                          id="inputUserEmailConfirmed"
-                          className="form-control"
-                          ref={c => {
-                            this.inputUserEmailConfirmed = c;
-                          }}
-                        >
-                          <option value="true">Yes</option>
-                          <option value="false">No</option>
-                        </select>
-                      </div>
-                    </div>
-                    {/* status */}
-                    <div className="form-group">
-                      <label
-                        htmlFor="inputUserStatus"
-                        className="col-sm-2 control-label"
-                      >Status</label>
-                      <div className="col-sm-10">
-                        <select
-                          id="inputUserStatus"
-                          className="form-control"
-                          ref={c => {
-                            this.inputUserStatus = c;
-                          }}
-                        >
-                          <option value="active">Active</option>
-                          <option value="inactive">Inactive</option>
-                        </select>
-                      </div>
-                    </div>
-                  </div>
-                  {/* /.box-body */}
-                  <div className="box-footer">
-                    <Link
-                      to="/resource/user" className="btn btn-app pull-right"
-                    ><i className="fa fa-undo" /> Cancel</Link>
-                    <Link
-                      to="/resource/user" className="btn btn-app pull-right"
-                      onClick={event => this.deleteUser(event)}
-                    ><i className="fa fa-trash-o" /> Delete</Link>
-                    {/* eslint-disable jsx-a11y/no-static-element-interactions */}
-                    <a
-                      className="btn btn-app pull-right"
-                      onClick={event => this.updateUser(event)}
-                    ><i className="fa fa-floppy-o" /> Save</a>
-                    {/* eslint-enable jsx-a11y/no-static-element-interactions */}
-                  </div>
-                  {/* /.box-footer */}
-                </form>
+                <UpdateUserForm
+                  userId={userId}
+                  roleList={roles.list}
+                  userEditing={users.editing}
+                  updateUser={updateUser}
+                  deleteUser={deleteUser}
+                />
               </div>
               {/* /.col */}
             </section>
